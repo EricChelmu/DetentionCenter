863a329719be33d5386cec784b10cb9ccfc2366b
Eric Chelmu <eric.chelmu12@gmail.com>
2024-10-03 11:50:40 +0200

    input added

---
+++ DetentionCenter/Assets/Scripts/Character.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace player
+{
+    public class Character : MonoBehaviour
+    {
+        private InputAction moveAction;
+        private InputAction lookAction;
+        private Vector2 moveInput;
+        private Vector2 lookInput;
+
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
